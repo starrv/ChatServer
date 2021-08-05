66fae2a658acf7557bb23019310508e3ad68a2fe
Valencia <starrv2011@gmail.com>
2021-08-05 00:38:28 -0400

    changes

--- src/main/app.java
+++ src/main/app.java
@@ -4,7 +4,7 @@ public class app {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ChatServer server=new ChatServer(8080);
+		ChatServer server=new ChatServer(Integer.parseInt((args[0])));
 	}
 
 }
